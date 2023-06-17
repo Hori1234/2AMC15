a14818bc14ae24fcbf9bdf77c9026a4317107a20
20169341 <h.breazu@student.tue.nl>
2023-06-17 03:26:49 +0200

    .

--- agents/deep_q_agent.py
+++ agents/deep_q_agent.py
@@ -92,7 +92,7 @@ class DeepQAgent(BaseAgent):
         self.first_run = True
 
         # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.device = torch.device("cuda")
+        self.device = torch.device("cpu")
 
         print("the test will be run on: ", self.device)
         print("CUda current device: ", torch.cuda.current_device())
